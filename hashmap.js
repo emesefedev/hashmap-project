5790062f7c4833929dd80f647ecda7e5766f435c
emesefedev <emesefedev@gmail.com>
2024-11-14 14:33:06 +0100

    feature: all functions and introduction of linked lists

--- hashmap.js
+++ hashmap.js
@@ -1,14 +1,53 @@
+import { linkedList } from "./linked-lists.js"
+
+// TODO: Big Todo --> Guardar directamente todo como linkedlist
+
 class HashMap {
     constructor(loadFactor = 0.8, capacity = 16) {
         this.loadFactor = loadFactor;
         this.capacity = capacity;
+        
         this.buckets = []
+        this.totalItems = 0
     }
+
+    // Helpers
+
+    add(key, value, index) {
+        if (index < 0 || index >= this.capacity) {
+            throw new Error("Trying to access index out of bounds");
+        }
+
+        this.totalItems++
+        this.doubleCapacity()
+
+        this.buckets[index] = {key, value}
+    }
+
+    delete(index) {
+        if (index < 0 || index >= this.capacity) {
+            throw new Error("Trying to access index out of bounds");
+        }
+
+        this.totalItems--
+        this.buckets[index] = undefined
+    }
+
+    doubleCapacity() {
+        if (this.totalItems >= this.capacity * this.loadFactor) {
+            this.capacity *= 2
+            console.log("It's time to double the capacity")
+
+            // TODO: reasign elements
+        }
+    }
+
+    // Functions
     
     hash(key) {
         let hashCode = 0;
       
-        const primeNumber = 67;
+        const primeNumber = 17;
         for (let i = 0; i < key.length; i++) {
             hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
         }
@@ -18,50 +57,95 @@ class HashMap {
 
     set(key, value) {
         const hashCode = this.hash(key)
-        this.add(key, value, hashCode)
-    }
 
-    add(key, value, index) {
-        if (index < 0 || index >= this.buckets.length) {
-            throw new Error("Trying to access index out of bounds");
+        if (!this.has(key)) {
+            this.add(key, value, hashCode)
+            return
+        } 
+        
+        const keyValue = this.buckets[hashCode] // TODO: comprobar que esto no da error
+        if (keyValue.key === key) {
+            keyValue.value = value
+            return
         }
 
-        this.buckets[index] = [key, value]
+        console.log(`!!! CONFLICT !!! ${key} - ${keyValue[0]}`)
+        const list = linkedList()
+        list.append(keyValue)
+        list.append({key, value})
+        this.buckets[hashCode] = list
     }
 
     get(key) {
+        const hashCode = this.hash(key)
 
+        return this.has(key) ? this.buckets[hashCode].value : null
     }
 
     has(key) {
+        const hashCode = this.hash(key)
+        const keyValue = this.buckets[hashCode]
 
+        // TODO: Esto no es cierto, ¿qué pasa si hay linkedList?
+        return keyValue !== undefined
     }
 
     remove(key) {
+        if (this.has(key)) {
+            // TODO: Esto no es tan simple, ¿qué pasa si hay linkedList?
+            this.delete(this.hash(key))
+            return true
+        }
 
+        return false
     }
 
     get length() {
-
+        return this.totalItems
     }
 
     clear() {
-
+        for (const key of this.keys) {
+            this.remove(key)
+        }
     }
 
     get keys() {
+        const keys = []
+        for (const keyValue of this.entries) {
+            keys.push(keyValue.key)
+        }
 
+        return keys
     }
 
     get values() {
+        const values = []
+        for (const keyValue of this.entries) {
+            values.push(keyValue.value)
+        }
 
+        return values
     }
 
     get entries() {
+        const entries = []
+        for (const keyValue of this.buckets) {
+            if (keyValue !== undefined) {
+                entries.push(keyValue)
+            }
+        }
 
+        return entries
     }
 
 }
 
 const test = new HashMap(0.8, 16)
-console.log(test.hash("miquel"))
+test.set('apple', 'red')
+test.set('banana', 'yellow')
+test.set('carrot', 'orange')
+test.set('dog', 'brown')
+test.set('elephant', 'gray')
+test.set('frog', 'green')
+console.log(test.entries[0].head().value)
---
+++ linked-lists.js
@@ -0,0 +1,145 @@
+export function linkedList () {
+    let _head = null
+    let _tail = null
+    
+    let _size = 0
+
+    const append = (value) => {
+        const newNode = node(value)
+        if (_size === 0) {
+            _head = newNode
+            _tail = newNode
+        } else if (_size === 1) {
+            _head.updateNextNode(newNode)
+            _tail = newNode
+        } else {
+            _tail.updateNextNode(newNode)
+            _tail = newNode
+        }
+
+        _size++
+    }
+
+    const prepend = (value) => {
+        const prevHead = _head
+        _head = node(value, prevHead)
+
+        _size++
+    }
+
+    const at = (index) => {
+        
+        if (index < 0 || index >= _size) throw Error("Index Out Of Bounds")
+
+        let i = 0
+        let node = _head
+        while (i < index) {
+            node = node.next()
+            i++
+        }
+
+        return node
+    }
+
+    const pop = () => {
+        if (_size <= 0) return
+
+        if (_size > 1) {
+            _tail = at(_size - 2)
+            _tail.updateNextNode(null)
+        } else {
+            _head = null
+            _tail = null
+        }
+        
+        _size--
+    }
+
+    const containsAt = (value) => {
+        for (let i = 0; i < _size; i++) {
+            if (at(i).value() === value) {
+                return {contains: true, at: i}
+            }
+        }
+        return {contains: false, at: -1}
+    }
+
+    const contains = (value) => {
+        return containsAt(value).contains
+    }
+
+    const find = (value) => {
+        return containsAt(value).at
+    }
+
+    const toString = () => {
+        if (_size > 0) {
+            console.log(nodeToString(_head))
+        } else {
+            console.log("null")
+        }
+    }
+
+    const nodeToString = (node) => {
+        if (node.next() === null) {
+            return `( ${node.value()} ) -> null`
+        }
+        return `( ${node.value()} ) -> ${nodeToString(node.next())}`
+    }
+
+    const insertAt = (value, index) => {
+        if (index < 0 || index >= _size) throw Error("Index Out Of Bounds")
+
+        if (index === 0) {
+            prepend(value)
+        } else {
+            const nodeAtIndex = at(index)
+            const newNode = node(value, nodeAtIndex)
+            at(index - 1).updateNextNode(newNode)
+            
+            _size++
+        }
+    }
+
+    const removeAt = (index) => {
+        if (_size <= 0) return
+        if (index < 0 || index >= _size) throw Error("Index Out Of Bounds")
+        
+        if (_size > 1) {
+            if (index === 0) {
+                _head = _head.next()
+            } else {
+                const nodeToRemove = at(index)
+                at(index - 1).updateNextNode(nodeToRemove.next())
+            }
+            _size--
+        } else {
+            pop()
+        }        
+    }
+
+    const size = () => _size
+    const head = () => _head
+    const tail = () => _tail
+
+    return {toString, append, prepend, size, head, tail, at, pop, contains, find, insertAt, removeAt}
+}
+
+function node (val = null, nextNode = null) {
+
+    let _value = val
+    let _nextNode = nextNode
+
+    const updateValue = (newValue) => {
+        _value = newValue
+    }
+
+    const updateNextNode = (newNext) => {
+        _nextNode = newNext
+    }
+
+    const value = () => _value
+    const next = () => _nextNode
+
+    return {value, next, updateValue, updateNextNode}  
+}
